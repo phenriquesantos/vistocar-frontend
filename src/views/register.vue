dfa7eb395e1b7edc07b6558b4a0274469ac8a4ad
Pedro Henrique Santana dos Santos <pedro.santana@ai4.com.br>
2020-03-06 16:34:11 -0300

    User register

---
+++ src/views/register.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  
+}
+</script>
+
+<template>
+  <div class="register">
+    <h1 class="register__title">Cadastro</h1>
+
+    <form action="" class="register__form">
+      <div class="row">
+        <div class="col">
+          <label for="txt_first-name">Nome</label>
+          <input type="text" name="first-name" placeholder="Nome" id="txt_first-name" />
+
+          <label for="txt_last-name">Sobrenome</label>
+          <input type="text" name="" id="txt_last-name" placeholder="Sobrenome" />
+        </div>
+      </div><!-- row -->
+
+      <div class="row">
+        <div class="col">
+          <label for="txt_cpf">CPF</label>
+          <input type="text" name="cpf" placeholder="CPF" id="txt_cpf" />
+        </div>
+        <div class="col">
+          
+        </div>
+      </div>
+
+
+    </form>
+  </div>
+</template>
+
+<style lang="less" scoped>
+
+</style>
