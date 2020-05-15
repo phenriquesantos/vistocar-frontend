709a4c765b8d242223c7184e5bffddb387e7d4de
Pedro Henrique <pedro.progsantos@gmail.com>
2020-05-15 00:16:19 -0300

    Update Resgister.vue

--- src/views/register.vue
+++ src/views/register.vue
@@ -1,11 +1,89 @@
 <script>
-import pages from '@/components/common/pages.vue';
+import axios from '@/plugins/axios';
 import { TheMask } from 'vue-the-mask';
 
+import pages from '@/components/common/pages.vue';
+import error from '@/components/common/error.vue';
+
 export default {
+  created(){
+    if(localStorage.user){
+      delete localStorage.user;
+    }
+  },
+
   components: {
     'page-default': pages,
-    'the-mask': TheMask
+    'the-mask': TheMask,
+    'error': error
+  },
+
+  data() {
+    return {
+      user:{
+        firstName: '',
+        lastName: '',
+        cpf: '',
+        rgNumber: '',
+        rgUf: '',
+        email: '',
+        password: '',
+        repeatPassword: '',
+      },
+      error: ''
+    }
+  },
+
+  methods: {
+    async sendForm(e){
+      e.preventDefault();
+
+      if(this.user.password != this.user.repeatPassword){
+        this.error = 'Senhas não coincidem';
+      }
+      
+      try{
+        await this.createUser();
+        await this.createClient();
+
+        this.$router.push('/login')
+      }catch(e){
+        console.log(`ERROR ${e.code} - ${e.message}`);
+        this.error = 'Erro ao completar requisição tente novamente mais tarde.';
+      }
+    },
+
+    async createUser(){
+      await axios({
+        method: 'POST',
+        url: '/user',
+        data: {
+          'first_name': this.user.firstName,
+          'last_name': this.user.lastName,
+          'email': this.user.email,
+          'active': true,
+          'role': 'client',
+          'password': this.user.password,
+        }
+      });
+    },
+
+    async createClient(){
+      await axios({
+        method: 'POST',
+        url: '/client',
+        data: {
+          'first_name': this.user.firstName,
+          'last_name': this.user.lastName,
+          'cpf': this.user.cpf,
+          'rg_number': this.user.rgNumber,
+          'rg_uf': this.user.rgUf,
+          'email': this.user.email,
+          'active': true,
+          'password': this.user.password,
+        }
+      });
+    }
   }
 }
 </script>
@@ -15,44 +93,50 @@ export default {
 
     <div class="register">
       <div class="container">
-        <form action="" class="register__form">
-        <h1 class="register__form__title">Cadastro de Usuário</h1>
+        <form action="" class="register__form" v-on:submit="sendForm($event)" >
+          <div class="register__form__error" v-if="error != ''">
+            <error
+              v-bind:error="error"
+            />
+          </div>
+
+          <h1 class="register__form__title">Cadastro de Usuário</h1>
           <div class="row">
             <div class="col-md-5">
               <label for="txt_first-name">Nome</label>
-              <input type="text" name="first-name" placeholder="Nome" id="txt_first-name" required />
+              <input type="text" name="first-name" placeholder="Nome" id="txt_first-name" required v-model="user.firstName" />
             </div><!-- col md 4 -->
             <div class="col-md-7">
               <label for="txt_last-name">Sobrenome</label>
-              <input type="text" name="last-name" placeholder="Sobrenome" id="txt_last-name" required />
+              <input type="text" name="last-name" placeholder="Sobrenome" id="txt_last-name" required v-model="user.lastName"/>
             </div><!-- col md 7 -->
           </div><!-- row -->
 
           <div class="row">
             <div class="col-md-6">
               <label for="txt_phone">Celular</label>
-              <input type="text" name="phone" placeholder="Celular" id="txt_phone" required />
+              <the-mask type="text" name="phone" placeholder="Celular" id="txt_phone" required v-bind:mask="['(##) #.####-####']" />
             </div><!-- col md 6 -->
 
             <div class="col-md-6">
               <label for="txt_email">E-mail</label>
-              <input type="email" name="email" placeholder="E-mail" id="txt_email" required />
+              <input type="email" name="email" placeholder="E-mail" id="txt_email" required v-model="user.email" />
             </div><!-- col md 6 -->
           </div><!-- row -->
 
           <div class="row">
             <div class="col-md-6">
               <label for="txt_cpf">CPF</label>
-              <the-mask type="text" name="cpf" placeholder="CPF" id="txt_cpf" required v-bind:mask="['###.###.###-##']" />
+              <the-mask type="text" name="cpf" placeholder="CPF" id="txt_cpf" required v-bind:mask="['###.###.###-##']" v-model="user.cpf" />
             </div><!-- col md 6 -->
 
             <div class="col-md-5">
               <label for="txt_rg-number">Número do RG</label>
-              <the-mask type="text" name="rg-number" placeholder="Número do RG" id="txt_rg-number" v-bind:mask="['##.###.###-#']" />
+              <the-mask type="text" name="rg-number" placeholder="Número do RG" id="txt_rg-number" v-bind:mask="['##.###.###-#']" v-model="user.rgNumber" />
             </div><!-- col md 5 -->
             <div class="col-md-1">
               <label for="sel_rg-uf">UF do RG</label>
-              <select name="rg-uf" id="sel_rg-uf">
+              <select name="rg-uf" id="sel_rg-uf" v-model="user.rgUf">
                 <option value="default" disabled selected>--</option>
                 <option value="AC">AC</option>
                 <option value="AL">AL</option>
@@ -88,11 +172,11 @@ export default {
           <div class="row">
             <div class="col-md-6">
               <label for="txt_pass">Senha</label>
-              <input type="password" name="pass" placeholder="Senha" id="txt_pass" required />
+              <input type="password" name="pass" placeholder="Senha" id="txt_pass" required v-model="user.password" />
             </div><!-- col md 6 -->
             <div class="col-md-6">
               <label for="txt_r-pass">Repita a senha</label>
-              <input type="password" name="pass" placeholder="Repita a senha" id="txt_r-pass" required />
+              <input type="password" name="pass" placeholder="Repita a senha" id="txt_r-pass" required v-model="user.repeatPassword" />
             </div>
           </div>
 
@@ -119,6 +203,10 @@ export default {
       content: ' ';
       display: block;
       clear: both;
+    }
+
+    &__error{
+      margin-bottom: 20px;
     }
 
     &__title{
